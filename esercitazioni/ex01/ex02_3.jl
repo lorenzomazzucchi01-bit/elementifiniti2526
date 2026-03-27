a4409b553a486b45cefd16cd110915341b7632d8
Ivan Bioli <91244037+IvanBioli@users.noreply.github.com>
2026-03-27 10:10:12 +0100

    Add ex02_3.JL

--- esercitazioni/ex01/ex02_3.jl
+++ esercitazioni/ex01/ex02_3.jl
@@ -6,7 +6,7 @@ using Revise
 includet("../../modules/Meshing.jl")
 using Plots
 
-function add_triangle(p1, p2, p3)
+function add_triangle!(p1, p2, p3)
     x = [p1[1], p2[1], p3[1], p1[1]]  # x-coordinates (closed loop)
     y = [p1[2], p2[2], p3[2], p1[2]]  # y-coordinates (closed loop)
     plot!(x, y, label="", linewidth=1, color=:black)
@@ -15,11 +15,13 @@ end
 # Function to plot the mesh
 function plot_mesh(T, p)
     fig = plot() # Initialize an empty plot
-    
-    ###################################
-    ########## PLOT THE MESH ##########
-    ####################################
-
+    for j = 1:size(T, 2)
+        v1 = p[:,T[1,j]]
+        v2 = p[:,T[2,j]]
+        v3 = p[:,T[3,j]]
+        add_triangle!(v1, v2, v3)
+    end
+    plot!(fig, aspect_ratio=1)
     return fig 
 end
 
@@ -30,7 +32,6 @@ T, p = get_nodes_connectivity(out_file)
 
 # Plot the mesh
 fig = plot_mesh(T, p)
-plot!(fig, aspect_ratio=1)
 savefig("figures/ex02_1.pdf")
 display(fig)
 
